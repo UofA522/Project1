d61a53cf400a7dee30078626cd0ebf66896d20d0
Alton Dsouza <altondsouza02@gmail.com>
2024-10-10 11:57:51 -0600

    Added Version

--- stock_market/src/main.rs
+++ stock_market/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(version="0.1.0", about, long_about = None)]
+struct Stock {
+    #[arg(short, long)]
+    name : String
 }
+fn main() {
+    let stock_args = Stock::parse();
+    println!("Stock:{}",stock_args.name);
+}
+
+
