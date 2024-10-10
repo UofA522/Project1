14058df11d04bf16c49a7be45472771bab180a7b
Alton Dsouza <altondsouza02@gmail.com>
2024-10-10 12:22:13 -0600

    Added Yahoo Finance Crate

--- stock_market/src/main.rs
+++ stock_market/src/main.rs
@@ -3,14 +3,19 @@ use clap::Parser;
 
 
 #[derive(Parser, Debug)]
-#[command(version="0.1.0", about, long_about = None)]
+#[command(version="0.1.0", about="CLI Program to fetch stock using Yahoo finance", long_about = "CLI Program to fetch stock using Yahoo finance. The CLI is build using the clap crate in rust")]
 struct Stock {
+    /// Short name of a stock.
     #[arg(short, long)]
     name : String
 }
 fn main() {
     let stock_args = Stock::parse();
-    println!("Stock:{}",stock_args.name);
+    fetch_stock(stock_args.name)
+}
+
+fn fetch_stock(stock_name: String) {
+
 }
 
 
