d0679ebebeab15af57e6149c042d19af9fab529a
Alton Dsouza <altondsouza02@gmail.com>
2024-10-10 12:51:19 -0600

    Implemented lazy singleton pattern for yahoo connector

--- stock_market/src/main.rs
+++ stock_market/src/main.rs
@@ -4,6 +4,13 @@ use yahoo_finance_api as yahoo;
 
 use lazy_static::lazy_static;
 use std::sync::Mutex;
+use yahoo_finance_api::YahooConnector;
+
+// Implements lazy singleton pattern for Yahoo Connector
+lazy_static! {
+    static ref CONNECTOR: Mutex<YahooConnector> = Mutex::new(YahooConnector::new().expect("Unable to Create a Yahoo Connector"));
+}
+// Creating a parser for the CLI program that takes in the name of the stock
 #[derive(Parser, Debug)]
 #[command(version="0.1.0", about="CLI Program to fetch stock using Yahoo finance", long_about = "CLI Program to fetch stock using Yahoo finance. The CLI is build using the clap crate in rust")]
 struct Stock {
@@ -11,14 +18,13 @@ struct Stock {
     #[arg(short, long)]
     name : String
 }
-#[tokio::main]
-async fn main() {
+
+fn main() {
     let stock_args = Stock::parse();
-    let provider = yahoo::YahooConnector::new().expect("Failed to Create Yahoo connector");
 }
 
 async fn fetch_stock(stock_name: String) {
-
+    CONNECTOR.lock().unwrap().get_latest_quotes()
 }
 
 
